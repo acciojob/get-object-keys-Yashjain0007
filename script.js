c6e0fc5d4c0844b29f7f70a686aa49414ae31add
Yash Jain <124593014+Yashjain0007@users.noreply.github.com>
2023-05-27 14:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
 const student = {
-	name: "yash",
+	name: "yash"
 }
-
+Object.prototype.getKeys = function() {
+	return Object.keys(this);
+};
 Object.prototype.getkeys = function () {
 	return Object.keys(this);
 } 
